ba8e3fe93a72976c545d7aba82a60f270159c7c3
Kat Vengo <katvengo@gmail.com>
2019-04-09 18:53:54 -0700

    working

---
+++ assets/javascript/project.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function (response) {
+        console.log(response)
+    });
+
+
+
+})
